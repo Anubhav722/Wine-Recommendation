e82da25b8eb869201af7f7a4a72ff36a5682e2e7
Anubhav_722 <anubhavs286@gmail.com>
2017-04-14 13:39:31 +0530

    add views

--- winerama/reviews/admin.py
+++ winerama/reviews/admin.py
@@ -3,4 +3,14 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
+from .models import Wine, Review
 # Register your models here.
+
+class ReviewAdmin(admin.ModelAdmin):
+    model = Review
+    list_display = ('wine', 'rating', 'user_name', 'comment', 'pub_date')
+    list_filter = ('pub_date', 'user_name')
+    search_fields = ('comment', 'user_name', )
+
+admin.site.register(Wine)
+admin.site.register(Review, ReviewAdmin)
