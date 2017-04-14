d5671311494665d42d10da60f58b1ae44b7b7760
Anubhav_722 <anubhavs286@gmail.com>
2017-04-14 14:50:28 +0530

    add view for adding review

--- winerama/reviews/urls.py
+++ winerama/reviews/urls.py
@@ -5,9 +5,11 @@ urlpatterns = [
     # ex: /
     url(r'^$', views.review_list, name='review_list'),
     # ex: /review/5/
-    url(r'^review/(?P<review_id>[0-9]+)/$', views.review_detail, name='review_detail'),
+    url(r'^(?P<review_id>[0-9]+)/$', views.review_detail, name='review_detail'),
     # ex: /wine/
-    url(r'^wine$', views.wine_list, name='wine_list'),
+    url(r'^wine/$', views.wine_list, name='wine_list'),
     # ex: /wine/5/
     url(r'^wine/(?P<wine_id>[0-9]+)/$', views.wine_detail, name='wine_detail'),
+
+    url(r'^add_review/(?P<wine_id>[0-9]+)/$', views.add_review, name='add_review'),
 ]
--- winerama/reviews/views.py
+++ winerama/reviews/views.py
@@ -2,8 +2,12 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
 
 from .models import Wine, Review
+from .forms import ReviewForm
+import datetime
 # Create your views here.
 
 def review_list(request):
@@ -27,3 +31,24 @@ def wine_list(request):
 def wine_detail(request, wine_id):
     wine = get_object_or_404(Wine, pk=wine_id)
     return render(request, 'reviews/wine_detail.html', {'wine': wine})
+
+def add_review(request, wine_id):
+    wine = get_object_or_404(Wine, pk=wine_id)
+    form = ReviewForm(request.POST)
+
+    if form.is_valid():
+        rating = form.cleaned_data['rating']
+        comment = form.cleaned_data['comment']
+        user_name = form.cleaned_data['user_name']
+        review = Review()
+        review.wine = wine
+        review.user_name = user_name
+        review.rating = rating
+        review.comment = comment
+        review.pub_date = datetime.datetime.now()
+        review.save()
+        # import ipdb; ipdb.set_trace()
+
+        return HttpResponseRedirect(reverse('reviews:review_list'))#, args=(wine.id,)))
+
+    return render(request, 'reviews/wine_detail.html', {'wine':wine, 'form':form})
