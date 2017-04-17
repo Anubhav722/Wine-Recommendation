9d5f6493bfb99e17dbb6be8e1fcc286fce50f8d8
Anubhav_722 <anubhavs286@gmail.com>
2017-04-17 13:58:52 +0530

    add model Cluster and register it in admin

--- winerama/reviews/admin.py
+++ winerama/reviews/admin.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-from .models import Wine, Review
+from .models import Wine, Review, Cluster
 # Register your models here.
 
 class ReviewAdmin(admin.ModelAdmin):
@@ -12,5 +12,10 @@ class ReviewAdmin(admin.ModelAdmin):
     list_filter = ('pub_date', 'user_name')
     search_fields = ('comment', 'user_name', )
 
+class ClusterAdmin(admin.ModelAdmin):
+    model = Cluster
+    list_display = ['name', 'get_members']
+
 admin.site.register(Wine)
 admin.site.register(Review, ReviewAdmin)
+admin.site.register(Cluster, ClusterAdmin)
